dc3c14ec150fc8ea07a7a59e06e378896b3ee3de
Scala Steward <scala_steward@virtuslab.com>
2024-03-18 16:17:27 +0000

    Reformat with scalafmt 3.8.0
    Executed command: scalafmt --non-interactive

--- src/main/scala/org/felher/magicvar/Context.scala
+++ src/main/scala/org/felher/magicvar/Context.scala
@@ -1,5 +1,5 @@
 package org.felher.magicvar
 
 final case class Context(path: List[String]):
-  def / (segment: String): Context = Context(path :+ segment)
-  def / (index: Int): Context = Context(path :+ index.toString)
+  def /(segment: String): Context = Context(path :+ segment)
+  def /(index: Int): Context      = Context(path :+ index.toString)
